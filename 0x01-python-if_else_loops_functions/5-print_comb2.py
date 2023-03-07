28f67b43f5b991945ccd25d74d2e738398cc44c1
hailu-mengesha <hailemariam0303@gmail.com>
2023-03-07 09:09:43 -0800

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(0,10):
-    for j in range(0,10):
-        print("{}{}".format(i,j),end=", ")
+for i in range(0,100):
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02}".format(i), end=", ")
