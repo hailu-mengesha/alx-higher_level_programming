74e85c768f1f096f6001c4a793501d74974beac0
hailu-mengesha <hailemariam0303@gmail.com>
2023-03-27 11:07:29 -0700

    ByteCode -> Python #4

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-
-
 def magic_calculation(a, b):
-    const = 98
-    result = pow(a, b) + const
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            result += (a ** b) / i
+        except Exception:
+            result = b + a
+            break
     return result
